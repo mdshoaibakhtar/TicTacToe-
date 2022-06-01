8ad434ac25e2e2847c5eb5d55e22b679752618ec
Md Shoaib Akhtar <mdshoaibakhtar966@gmail.com>
2022-06-01 16:47:49 +0530

    fixing some bugs

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ let turn = "X"
 let isgameover = false;
 
 // Function to change the turn
-const changeTurn = ()=>{
-    return turn === "X"? "0": "X"
+const changeTurn = () => {
+    return turn === "X" ? "0" : "X"
 }
 
 // Function to check for a win
-const checkWin = ()=>{
+const checkWin = () => {
     let boxtext = document.getElementsByClassName('boxtext');
     let wins = [
         [0, 1, 2, 5, 5, 0],
@@ -23,45 +23,66 @@ const checkWin = ()=>{
         [0, 4, 8, 5, 15, 45],
         [2, 4, 6, 5, 15, 135],
     ]
-    wins.forEach(e =>{
-        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "") ){
+    wins.forEach(e => {
+        if ((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "")) {
             document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won"
             isgameover = true
             document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
-            document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
-            document.querySelector(".line").style.width = "20vw";
+            // document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
+            // document.querySelector(".line").style.width = "20vw";
         }
     })
 }
 
+
+
+    // Add onclick listener to reset button
+    reset.addEventListener('click', () => {
+        let boxtexts = document.querySelectorAll('.boxtext');
+        Array.from(boxtexts).forEach(element => {
+            element.innerText = ""
+        });
+        turn = "X";
+        isgameover = false
+        document.querySelector(".line").style.width = "0vw";
+        document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+        document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px"
+    })
+
+    const restart=()=>{
+        let boxtexts = document.querySelectorAll('.boxtext');
+        Array.from(boxtexts).forEach(element => {
+            element.innerText = ""
+        });
+        turn = "X";
+        isgameover = false
+        document.querySelector(".line").style.width = "0vw";
+        document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+        document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px"
+    }
+
 // Game Logic
 // music.play()
 let boxes = document.getElementsByClassName("box");
-Array.from(boxes).forEach(element =>{
+Array.from(boxes).forEach(element => {
     let boxtext = element.querySelector('.boxtext');
-    element.addEventListener('click', ()=>{
-        if(boxtext.innerText === ''){
+    element.addEventListener('click', () => {
+
+        console.log('click hu hai');
+        if (isgameover) {
+            restart();
+        }
+        if (boxtext.innerText === '') {
             boxtext.innerText = turn;
             turn = changeTurn();
             audioTurn.play();
             checkWin();
-            if (!isgameover){
-                document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
-            } 
+            if (!isgameover) {
+                document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+            }
         }
     })
 })
 
-// Add onclick listener to reset button
-reset.addEventListener('click', ()=>{
-    let boxtexts = document.querySelectorAll('.boxtext');
-    Array.from(boxtexts).forEach(element => {
-        element.innerText = ""
-    });
-    turn = "X"; 
-    isgameover = false
-    document.querySelector(".line").style.width = "0vw";
-    document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
-    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px"
-})
+
 
